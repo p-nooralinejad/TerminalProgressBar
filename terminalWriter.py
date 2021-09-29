b2ac10eeeef52b6179cfa9dd54ddff7701e4f31b
Luka Mamukashvili <57063715+UltraStudioLTD@users.noreply.github.com>
2021-09-29 10:53:36 +0400

    Corrected

--- terminalWriter.py
+++ terminalWriter.py
@@ -3,29 +3,31 @@ import os
 import sys
 import curses
 
+def send(seq: str):
+	sys.stdout.write(seq)
+	sys.stdout.flush()
+
 class terminalWriter:
 	ROWS=0
 	COLS = 0
 	
 	def __init__(self):
-		self.ROWS = os.get_terminal_size()[1]
-		self.COLS = os.get_terminal_size()[0]
+		self.COLS, self.ROWS = shutil.get_terminal_size()
 	
 	def get_current_position(self):
 		return 0
 
-	def write_progress_bar(self,Str):
-		print(Str)
-		print("\033[2A")
+	def write_progress_bar(self, value):
+		send("%s\033[2A" % value)
 
-	def write(self,x,y,Str):
-		print("\033[" + str(y) + ";" + str(x) + "H" + Str)
+	def write(self, x: int, y: int, value):
+		send("\033[%s;%sH%s" % (y, x, value))
 
 	def clear(self):
-		os.system("clear")
+		send("\033c")
 
 	def move_cursor(self,x,y):
-		print("\033[%d;%dH" % (y, x))
+		send("\033[%d;%dH" % (y, x))
 
 	def get_rows(self):
 		return self.ROWS;
@@ -34,4 +36,4 @@ class terminalWriter:
 		return self.COLS;
 
 	def done(self):
-		print("")
+		send("")
